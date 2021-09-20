d7d7da4282456d723573d92a73d92fb0fc5f5f0a
Saurabh Kumrr <hmsaurabhkr@gmail.com>
2021-09-20 13:55:02 +0530

    Add Sidenav bar with submenu

---
+++ script.js
@@ -0,0 +1,43 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+
+   /*===== SHOW NAVBAR  =====*/ 
+   const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    headerpd = document.getElementById(headerId)
+
+    // Validate that all variables exist
+    if(toggle){
+        toggle.addEventListener('click', ()=>{
+            // change icon
+            toggle.classList.toggle('bx-x')
+            
+        })
+    }
+}
+
+showNavbar('header-toggle','nav-bar','body-pd','header')
+
+/*===== LINK ACTIVE  =====*/ 
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(l=> l.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
